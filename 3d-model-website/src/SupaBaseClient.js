01659b2182d09f1d8c43b92b88679f56a139aee5
David Jansson <david.jansson@telia.com>
2025-10-26 12:55:47 +0100

    remove generated files

--- 3d-model-website/src/SupaBaseClient.js
+++ 3d-model-website/src/SupaBaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
-const supabaseUrl = 'https://fllpuuljzvxpsofbwqhi.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsbHB1dWxqenZ4cHNvZmJ3cWhpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0NTk1OTgsImV4cCI6MjA3NzAzNTU5OH0.LBZYqcPn4AHyXiMs4hTo-pnmcoH1W2EWkE0WrDqGATk'
+const supabaseUrl = 'https://yyy.supabase.co'
+const supabaseKey = 'zzzzz'
 const supabase = createClient(supabaseUrl, supabaseKey)
 
 export default supabase;
